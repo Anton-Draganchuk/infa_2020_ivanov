7bc1428a3a1eb13060fc68c951b0ca6b7c3d959c
Anton-Draganchuk <draganchuk.ar@phystech.edu>
2021-10-18 10:36:35 +0300

    The Best picture

--- lab3/2.py
+++ lab3/2.py
@@ -1,4 +1,3 @@
-import math
 import pygame
 from pygame.draw import *
 
@@ -7,54 +6,162 @@ pygame.init()
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
 
-
-rect(screen, (0, 0, 255), (0, 200, 400, 90))
-rect(screen, (255, 204, 0), (0, 290, 400, 110))
-rect(screen, (0, 255, 255), (0, 0, 400, 200))
-circle(screen, (255, 255, 0), (340, 60), 30)
-line(screen, (153, 51, 0), (100, 370), (100, 270), 10)
-polygon(screen, (153, 51, 102), [(30, 270), (170, 270), (100, 230), (30, 270)])
-
-x1 = 30
-x2 = 170
-N = 6
-color = (0, 0, 0)
-h = (x2 - x1) // (N + 1)
-x = x1 + h
-for i in range(N):
-    line(screen, color, (100, 230), (x, 270))
-    x += h
-
-rect(screen, (153, 51, 0), (250, 220, 80, 30))
-polygon(screen, (153, 51, 0), [(330, 220), (390, 220), (330, 250), (330, 220)])
-circle(screen, (153, 51, 0), (250, 220), 30)
-rect(screen, (0, 0, 255), (0, 200, 400, 20))
-rect(screen, (0, 255, 255), (0, 150, 400, 50))
-circle(screen, (255, 255, 255), (347, 230), 7)
-circle(screen, (0, 0, 0), (347, 230), 7, 2)
-line(screen, (0, 0, 0), (270, 220), (270, 120), 6)
-polygon(screen, (255, 255, 255), [(273, 120), (320, 170), (290, 170), (273, 120)])
-polygon(screen, (0, 0, 0), [(273, 120), (320, 170), (290, 170), (273, 120)], 1)
-polygon(screen, (255, 255, 255), [(273, 220), (320, 170), (290, 170), (273, 220)])
-polygon(screen, (0, 0, 0), [(273, 220), (320, 170), (290, 170), (273, 220)], 1)
-circle(screen, (255, 255, 255), (100, 40), 20)
-circle(screen, (51, 51, 51), (100, 40), 20, 1)
-circle(screen, (255, 255, 255), (80, 40), 20)
-circle(screen, (51, 51, 51), (80, 40), 20, 1)
-circle(screen, (255, 255, 255), (60, 40), 20)
-circle(screen, (51, 51, 51), (60, 40), 20, 1)
-circle(screen, (255, 255, 255), (50, 60), 20)
-circle(screen, (51, 51, 51), (50, 60), 20, 1)
-circle(screen, (255, 255, 255), (70, 60), 20)
-circle(screen, (51, 51, 51), (70, 60), 20, 1)
-circle(screen, (255, 255, 255), (90, 60), 20)
-circle(screen, (51, 51, 51), (90, 60), 20, 1)
-circle(screen, (255, 255, 255), (110, 60), 20)
-circle(screen, (51, 51, 51), (110, 60), 20, 1)
-circle(screen, (255, 255, 255), (120, 50), 20)
-circle(screen, (51, 51, 51), (120, 50), 20, 1)
+# Colors
+Sand = [218, 165, 32]
+Sea = [0, 139, 139]
+Deck = [139, 69, 19]
+Sail = [255, 255, 255]
+Mast = [0, 0, 0]
+Clouds = [245, 245, 245]
+Sun = [255, 165, 0]
+Sky = [224, 255, 255]
+Umbrella = [255, 105, 180]
+Stick = [160, 82, 45]
+Circuit = [0, 0, 0]
+Window = [255, 255, 255]
 
 
+def picture():
+    """
+    Рисует картину с морем и караблем
+    :return: Картина
+    """
+    Sky_size = 150
+    Sea_size = 150
+    Sand_size = 100
+    # sum of Sky_size, Sea_size, Sand_size should be equal Screen_size
+    landscape(Sky_size, Sea_size, Sand_size)
+
+    Sun_pos = [340, 60]
+    Sun_radius = 30
+    sun(Sun_pos, Sun_radius)
+
+    Cloud_pos = [100, 50]
+    cloud(Cloud_pos)
+
+    Ship_pos = [250, Sky_size + 50]
+    Ship_size = [150, 150]
+    ship(Ship_pos, Ship_size)
+
+    Umbrella_pos = [100, 250]
+    Umbrella_size = [100, 150]
+    umbrella(Umbrella_pos, Umbrella_size)
+
+
+def landscape(Sky_size, Sea_size, Sand_size):
+    """
+    Рисует задний фон, небо, море, песок
+    :param Sky_size: Ширина неба
+    :param Sea_size: Ширина Моря
+    :param Sand_size: Ширина Песка
+    :return: Рисунок заднего фона
+    """
+    rect(screen, Sky, (0, 0, 400, Sky_size))
+    rect(screen, Sea, (0, Sky_size, 400, Sea_size))
+    rect(screen, Sand, (0, Sky_size + Sea_size, 400, Sand_size))
+
+
+def sun(Sun_pos, Sun_radius):
+    """
+    Рисует солнце
+    :param Sun_pos: Координаты солнца
+    :param Sun_radius: Радиус солнца
+    :return: Рисунок солнца
+    """
+    circle(screen, Sun, Sun_pos, Sun_radius)
+
+
+def cloud(Cloud_pos):
+    """
+    Рисует облака
+    :param Cloud_pos: координата центра облака
+    :return: Рисунок облак
+    """
+    circle(screen, Clouds, (Cloud_pos[0] - 20, Cloud_pos[1]), 20)
+    circle(screen, Circuit, (Cloud_pos[0] - 20, Cloud_pos[1]), 20, 1)
+    circle(screen, Clouds, Cloud_pos, 20)
+    circle(screen, Circuit, Cloud_pos, 20, 1)
+    circle(screen, Clouds, (Cloud_pos[0] + 20, Cloud_pos[1]), 20)
+    circle(screen, Circuit, (Cloud_pos[0] + 20, Cloud_pos[1]), 20, 1)
+    circle(screen, Clouds, (Cloud_pos[0] + 30, Cloud_pos[1] + 20), 20)
+    circle(screen, Circuit, (Cloud_pos[0] + 30, Cloud_pos[1] + 20), 20, 1)
+    circle(screen, Clouds, (Cloud_pos[0] + 10, Cloud_pos[1] + 20), 20)
+    circle(screen, Circuit, (Cloud_pos[0] + 10, Cloud_pos[1] + 20), 20, 1)
+    circle(screen, Clouds, (Cloud_pos[0] - 10, Cloud_pos[1] + 20), 20)
+    circle(screen, Circuit, (Cloud_pos[0] - 10, Cloud_pos[1] + 20), 20, 1)
+    circle(screen, Clouds, (Cloud_pos[0] - 30, Cloud_pos[1] + 20), 20)
+    circle(screen, Circuit, (Cloud_pos[0] - 30, Cloud_pos[1] + 20), 20, 1)
+
+
+def ship(Ship_pos, Ship_size):
+    """
+    Рисует Корабль
+    :param Ship_pos: Координата корабля
+    :param Ship_size: Размеры прямоугольника, в который будет вписан корабль
+    :return: Рисунок корабля
+    """
+    rect(screen, Deck, (int(Ship_pos[0] - Ship_size[0] / 4), int(Ship_pos[1] + Ship_size[1] / 4),
+                        int(Ship_size[0] / 2), int(Ship_size[1] / 4)))
+    polygon(screen, Deck, [(int(Ship_pos[0] + Ship_size[0] / 4), int(Ship_pos[1] + Ship_size[1] / 4)),
+                           (int(Ship_pos[0] + Ship_size[0] / 2), int(Ship_pos[1] + Ship_size[1] / 4)),
+                           (int(Ship_pos[0] + Ship_size[0] / 4), int(Ship_pos[1] + Ship_size[1] / 2)),
+                           (int(Ship_pos[0] + Ship_size[0] / 4), int(Ship_pos[1] + Ship_size[1] / 4))])
+    circle(screen, Deck, (int(Ship_pos[0] - Ship_size[0] / 4), int(Ship_pos[1] + Ship_size[1] / 4)),
+           int(Ship_size[1] / 4), draw_bottom_left=True)
+    rect(screen, Mast, (int(Ship_pos[0] - Ship_size[0] / 5), int(Ship_pos[1] - Ship_size[1] / 2),
+                        int(Ship_size[0] / 15), int(Ship_size[1]*(3 / 4))))
+    polygon(screen, Sail, [(int(Ship_pos[0] - Ship_size[0] / 5 + Ship_size[0] / 15), int(Ship_pos[1] - Ship_size[1] / 2)),
+                     (int(Ship_pos[0] + Ship_size[0] / 4), int(Ship_pos[1] - Ship_size[1] / 8)),
+                     (int(Ship_pos[0] - Ship_size[0] / 5 + Ship_size[0] / 15), int(Ship_pos[1] + Ship_size[1] / 4)),
+                     (int(Ship_pos[0]), int(Ship_pos[1] - Ship_size[1] / 8)),
+                     (int(Ship_pos[0] - Ship_size[0] / 5 + Ship_size[0] / 15), int(Ship_pos[1] - Ship_size[1] / 2))])
+    polygon(screen, Circuit, [(int(Ship_pos[0] - Ship_size[0] / 5 + Ship_size[0] / 15), int(Ship_pos[1] - Ship_size[1] / 2)),
+                     (int(Ship_pos[0] + Ship_size[0] / 4), int(Ship_pos[1] - Ship_size[1] / 8)),
+                     (int(Ship_pos[0] - Ship_size[0] / 5 + Ship_size[0] / 15), int(Ship_pos[1] + Ship_size[1] / 4)),
+                     (int(Ship_pos[0]), int(Ship_pos[1] - Ship_size[1] / 8)),
+                     (int(Ship_pos[0] - Ship_size[0] / 5 + Ship_size[0] / 15), int(Ship_pos[1] - Ship_size[1] / 2))], 1)
+    line(screen, Circuit, (int(Ship_pos[0] + Ship_size[0] / 4), int(Ship_pos[1] - Ship_size[1] / 8)),
+         (int(Ship_pos[0]), int(Ship_pos[1] - Ship_size[1] / 8)), 1)
+    circle(screen, Window, (int(Ship_pos[0] + Ship_size[0] / 3), int(Ship_pos[1] + Ship_size[1] / 3)),
+           int(Ship_size[0] / 20))
+    circle(screen, Circuit, (int(Ship_pos[0] + Ship_size[0] / 3), int(Ship_pos[1] + Ship_size[1] / 3)),
+           int(Ship_size[0] / 20), 1)
+
+
+def umbrella(Umbrella_pos, Umbrella_size):
+    """
+    Рисует зонтик
+    :param Umbrella_pos: координата зонтика
+    :param Umbrella_size: Размеры прямоугольника, в который будет вписан зонтик
+    :return: Рисунок зонтика
+    """
+    rect(screen, Stick, (int(Umbrella_pos[0] - Umbrella_size[0] / 22), int(Umbrella_pos[1] - Umbrella_size[1] / 2),
+                         int(Umbrella_size[0] / 11), int(Umbrella_size[1])))
+    polygon(screen, Umbrella, [(int(Umbrella_pos[0] - Umbrella_size[0] / 22),
+                                int(Umbrella_pos[1] - Umbrella_size[1] / 2)),
+                               (int(Umbrella_pos[0] + Umbrella_size[0] / 22),
+                                int(Umbrella_pos[1] - Umbrella_size[1] / 2)),
+                               (int(Umbrella_pos[0] + Umbrella_size[0] / 2), int(Umbrella_pos[1] - Umbrella_size[1] / 4)),
+                               (int(Umbrella_pos[0] - Umbrella_size[0] / 2), int(Umbrella_pos[1] - Umbrella_size[1] / 4)),
+                               (int(Umbrella_pos[0] - Umbrella_size[0] / 22),
+                                int(Umbrella_pos[1] - Umbrella_size[1] / 2))])
+    N = 3
+    x_1 = int(Umbrella_pos[0] - Umbrella_size[0] / 22)
+    y_1 = int(Umbrella_pos[1] - Umbrella_size[1] / 2)
+    dx = int(Umbrella_size[0] / (2*N + 1))
+    y_2 = int(Umbrella_pos[1] - Umbrella_size[1] / 4)
+    x_2 = int(Umbrella_pos[0] - Umbrella_size[0] / 2)
+    # Number of stick
+    for i in range(0, N):
+        x_2 += dx
+        line(screen, Circuit, (x_1, y_1), (x_2, y_2), 1)
+    x_1 = int(Umbrella_pos[0] + Umbrella_size[0] / 22)
+    for i in range(0, N):
+        x_2 += dx
+        line(screen, Circuit, (x_1, y_1), (x_2, y_2), 1)
+
+
+picture()
 
 pygame.display.update()
 clock = pygame.time.Clock()

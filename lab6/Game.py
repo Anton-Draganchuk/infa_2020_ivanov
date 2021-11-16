8cc8400c7168f8d33946020ed0e162807bde813a
Anton-Draganchuk <draganchuk.ar@phystech.edu>
2021-11-16 21:30:33 +0300

    Trucks and Hostile tank

--- lab6/Game.py
+++ lab6/Game.py
@@ -6,12 +6,12 @@ FPS = 30
 
 RED = 0xFF0000
 BLUE = 0x0000FF
-YELLOW = 0xFFC91F
-GREEN = 0x00FF00
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
 MAGENTA = 0xFF03B8
 CYAN = 0x00FFCC
 BLACK = (0, 0, 0)
-WHITE = 0xFFFFFF
+WHITE = (255, 255, 255)
 GREY = 0x7D7D7D
 GAME_COLORS = [BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
@@ -44,11 +44,11 @@ class Ball:
         и стен по краям окна (размер окна 800х600).
         """
         self.x += self.vx
-        if math.fabs(HEIGHT - 100 - self.y) <= self.r - self.vy and self.vy < 0:
+        """if math.fabs(HEIGHT - 100 - self.y) <= self.r - self.vy and self.vy < 0:
             self.y = HEIGHT - 100 - self.r
             balls.remove(self)
-        else:
-            self.y -= self.vy
+        else:"""
+        self.y -= self.vy
         if self.live == 0:
             balls.remove(self)
         else:
@@ -68,6 +68,39 @@ class Ball:
             self.r,
             1
         )
+
+    def hit(self):
+        for t in trucks:
+            if (t.x <= self.x <= t.x + 100) and t.y <= self.y <= t.y + 53:
+                t.HP -= 2
+                balls.remove(self)
+        for r in interface.rocks:
+            if (r[0] <= self.x <= r[0] + 72) and r[1] <= self.y <= r[1] + 50:
+                balls.remove(self)
+        for t in tanks:
+            if t.stop:
+                if t.an != 0:
+                    b11 = (56*math.cos(t.an) + t.y) - math.tan(t.an)*(t.x - 56*math.sin(t.an))
+                    b10 = self.y - math.tan(t.an) * self.x
+                    b12 = (t.y - 56 * math.cos(t.an)) - math.tan(t.an) * (t.x + 56 * math.sin(t.an))
+                    b21 = (t.y - 20 * math.sin(t.an)) + (1 / math.tan(t.an)) * (t.x - 20 * math.cos(t.an))
+                    b20 = self.y + (1 / math.tan(t.an)*self.x)
+                    b22 = (t.y + 20 * math.sin(t.an)) + (1 / math.tan(t.an)) * (t.x + 20 * math.cos(t.an))
+                    if t.an < 0:
+                        if b11 > b10 > b12 and b21 > b20 > b22:
+                            t.HP -= 2
+                            balls.remove(self)
+                    else:
+                        if b11 > b10 > b12 and b21 < b20 < b22:
+                            t.HP -= 2
+                            balls.remove(self)
+                else:
+                    b11 = (56*math.cos(t.an) + t.y) - math.tan(t.an)*(t.x - 56*math.sin(t.an))
+                    b10 = self.y - math.tan(t.an) * self.x
+                    b12 = (t.y - 56 * math.cos(t.an)) - math.tan(t.an) * (t.x + 56 * math.sin(t.an))
+                    if b11 > b10 > b12 and t.x > self.x > t.x + 39:
+                        t.HP -= 2
+                        balls.remove(self)
 
 
 class Tank:
@@ -109,7 +142,6 @@ class Tank:
                 new_ball.vx = self.f2_power * math.sin(self.an)
                 new_ball.vy = self.f2_power * math.cos(self.an)
                 balls.append(new_ball)
-                self.f2_power = 45
                 self.reload = 30
 
     def targetting(self, event):
@@ -139,12 +171,20 @@ class Interface:
         self.screen = screen
         self.town_hall_x = 30
         self.town_hall_y = 50
+        self.town_hall_HP = 100
         self.reload_color = GREEN
         self.road_y = 70
         self.road_x = 100
+        self.rocks = [[200, 400], [400, 250], [650, 300]]
+        self.font = pygame.font.Font(None, 35)
+        self.color_font = GREEN
 
     def draw_town_hall(self):
         self.screen.blit(image_town_hall, (self.town_hall_x, self.town_hall_y))
+        if self.town_hall_HP <= 30:
+            self.color_font = RED
+        self.screen.blit(self.font.render(str(self.town_hall_HP), False, self.color_font),
+                         (self.town_hall_x + 61, self.town_hall_y + 180))
 
     def draw_reload(self):
         self.reload_color = GREEN
@@ -167,32 +207,141 @@ class Interface:
         self.road_x = 100
         self.road_y = 70
 
+    def draw_rock(self):
+        for r in self.rocks:
+            self.screen.blit(image_rock, (r[0], r[1]))
+
+    def hit(self):
+        global trucks
+        for t in trucks:
+            if t.x <= 150:
+                trucks.remove(t)
+                self.town_hall_HP -= 10
+
+
+class Truck:
+    def __init__(self, screen):
+        self.screen = screen
+        self.x = 800
+        self.y = 80
+        self.HP = 10
+        self.font = pygame.font.Font(None, 25)
+
+    def draw(self):
+        screen.blit(image_truck, (self.x, self.y))
+        screen.blit(self.font.render(str(self.HP), False, YELLOW), (self.x + 50, self.y - 5))
+        self.x -= 1
+
+
+class Hostile:
+    def __init__(self, screen):
+        self.screen = screen
+        self.x = 800
+        self.y = 200
+        self.HP = 20
+        self.power = 20
+        self.reload = 50
+        self.path = random.randint(300, 600)
+        self.stop = 0
+        self.an = math.pi / 2
+        self.font = pygame.font.Font(None, 25)
+
+    def draw(self):
+        if self.an > 0:
+            self.screen.blit(pygame.transform.rotate(image_hostile_tank, - self.an / math.pi * 180),
+                        (self.x - (113*math.sin(self.an) + 39*math.cos(self.an)) / 2,
+                         self.y - (113*math.cos(self.an) + 39*math.sin(self.an)) / 2))
+        else:
+            self.screen.blit(pygame.transform.rotate(image_hostile_tank, - self.an / math.pi * 180),
+                        (self.x - (-113*math.sin(self.an) + 39*math.cos(self.an)) / 2,
+                         self.y - (113*math.cos(self.an) - 39*math.sin(self.an)) / 2))
+        screen.blit(self.font.render(str(self.HP), False, YELLOW), (self.x - 7, self.y - 5))
+        if self.x >= self.path:
+            self.x -= 2
+        else:
+            self.stop = 1
+            if self.y - tank.y != 0:
+                self.an = math.atan((tank.x - self.x) / (self.y - tank.y))
+            elif tank.x > self.x:
+                self.an = math.pi / 2
+            elif tank.y < self.x:
+                self.an = -math.pi / 2
+
+    def fire(self):
+        if self.stop:
+            if self.reload == 0:
+                new_ball = Ball(self.screen)
+                new_ball.live = 60
+                new_ball.x = self.x - 56*math.sin(self.an)
+                new_ball.y = self.y + 56*math.cos(self.an)
+                new_ball.vx = - self.power * math.sin(self.an)
+                new_ball.vy = - self.power * math.cos(self.an)
+                balls.append(new_ball)
+                self.reload = 50
+            else:
+                self.reload -= 1
+
+
+class Enemies:
+    def __init__(self, screen):
+        self.screen = screen
+        self.time = 0
+
+    def truck_spawn(self):
+        if self.time == 0:
+            new_truck = Truck(self.screen)
+            trucks.append(new_truck)
+            self.time = random.randint(90, 300)
+        else:
+            self.time -= 1
+
+    def tank_spawn(self):
+        if len(tanks) == 0:
+            new_tank = Hostile(self.screen)
+            tanks.append(new_tank)
+
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
-image_background = pygame.transform.scale(pygame.image.load('background.jpg'), (800, 600))
+image_background = pygame.transform.scale(pygame.image.load('background.png'), (800, 600))
 image_tank = pygame.transform.scale(pygame.image.load('tank.png').convert_alpha(), (62, 69))
 image_town_hall = pygame.transform.scale(pygame.image.load('town hall.png').convert_alpha(), (162, 172))
 image_road = pygame.transform.scale(pygame.image.load('road.png').convert_alpha(), (100, 100))
+image_truck = pygame.transform.scale(pygame.image.load('truck.png').convert_alpha(), (100, 53))
+image_rock = pygame.transform.scale(pygame.image.load('rock.png').convert_alpha(), (72, 50))
+image_hostile_tank = pygame.transform.scale(pygame.image.load('hostile tank.png').convert_alpha(), (39, 113))
 
 balls = []
+trucks = []
+tanks = []
 tank = Tank(screen)
 interface = Interface(screen)
+enemies = Enemies(screen)
 
 finished = False
 
 while not finished:
     clock.tick(FPS)
 
+    enemies.truck_spawn()
+    enemies.tank_spawn()
     screen.blit(image_background, (0, 0))
     interface.draw_road()
     interface.draw_town_hall()
+    interface.draw_rock()
     interface.draw_reload()
+    interface.hit()
+    for t in trucks:
+        t.draw()
+    for t in tanks:
+        t.draw()
+        t.fire()
     tank.draw()
     for b in balls:
         b.draw()
+        b.hit()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == 27):
@@ -213,6 +362,12 @@ while not finished:
     tank.move()
     for b in balls:
         b.move()
+    for t in trucks:
+        if t.HP == 0:
+            trucks.remove(t)
+    for t in tanks:
+        if t.HP == 0:
+            tanks.remove(t)
 
     pygame.display.update()
 
